36358771858d9af688d50670e39f517668dd1388
Mladen Trickovic <48336740+satrain@users.noreply.github.com>
2020-09-17 21:52:54 +0200

    Latest Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,42 +7,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const divs = document.querySelector(".about-divs")
 
-    const play = document.querySelector(".play-btn")
-    const popUp = document.querySelector(".pop-up")
 
     // Event Listeners
     desc.addEventListener("click", function () {
         specs.classList.remove("selected")
         goodFor.classList.remove("selected")
         desc.classList.add("selected")
-
-        divs.classList.add("margin-left-1")
-        divs.classList.remove("margin-left-2", "margin-left-3")
+        divs.style.marginLeft = "0";
     })
 
     specs.addEventListener("click", function () {
         desc.classList.remove("selected")
         goodFor.classList.remove("selected")
         specs.classList.add("selected")
-
-        divs.classList.add("margin-left-2")
-        divs.classList.remove("margin-left-1", "margin-left-3")
+        divs.style.marginLeft = "-823px";
     })
 
     goodFor.addEventListener("click", function () {
         specs.classList.remove("selected")
         desc.classList.remove("selected")
         goodFor.classList.add("selected")
-
-        divs.classList.add("margin-left-3")
-        divs.classList.remove("margin-left-2", "margin-left-1")
+        divs.style.marginLeft = "-1646px";
     })
-
-    play.addEventListener("click", function () {
-        popUp.style.display = "block"
-    })
-    document.querySelector(".s5-cross").addEventListener("click", function () {
-        popUp.style.display = "none"
-    })
-
-})
+});
